31dd44dcb50a6c0ac9f18a08952d34c202170ad5
Juan Sebastian Ramirez <juanramirezcastaneda@gmail.com>
2022-12-09 09:36:39 -0500

    update jobs

--- src/app.js
+++ src/app.js
@@ -8,20 +8,61 @@ app.use(bodyParser.json());
 app.set('sequelize', sequelize)
 app.set('models', sequelize.models)
 
-/**
- * FIX ME!
- * @returns contract by id
- */
-
 app.get('/', (_, res) => {
     res.send('hello world');
 })
 
+// 4
+app.post('/jobs/:job_id/pay',getProfile ,async (req, res) => {
+    sequelize.transaction(async (t) => {
+        const {Profile, Job, Contract} = req.app.get('models')
+        const userId = req.profile.id
+        const { job_id } = req.params
+    
+        const job = await Job.findOne({where: {id: job_id}})
+        const contract = await Contract.findOne({where : {id: job.ContractId}})
+
+        if(job.paid == true){
+            res.json({message: "the job was already paid"})
+        }
+
+        const amountToPay = job.price
+
+        const client = await Profile.findOne(
+            {where: {
+                [Op.and]: [
+                    {id: userId},
+                    {type: 'client'}
+        ]}    })
+
+        const clientBalance = client.balance
+        
+        if(clientBalance < amountToPay) {
+            res.json({message: "the balance is less than the amount to paid"})
+        }
+        
+        const contractorId = contract.ContractorId
+        const contractor = await Profile.findOne({where : {id: contractorId}})
+
+        await Job.Update({paid: true}, {where: {id: job_id}, transaction: t})
+        await Profile.Update(
+            {balance: clientBalance - amountToPay},
+            {where: {id: userId}, transaction: t})
+
+        await Profile.Update(
+            {balance: contractor.balance + amountToPay},
+            {where: {id: contractorId}, transaction: t})
+        
+        res.json({message: "the job has been paid"})
+    })
+})
+
+// 3
 app.get('/jobs/unpaid',getProfile ,async (req, res) =>{
     const {Contract, Job} = req.app.get('models')
     const userId = req.profile.id
 
-    const contracts = await Job.findAll({
+    const jobs = await Job.findAll({
         where: {
             paid: {
                 [Op.not]: true
@@ -39,11 +80,11 @@ app.get('/jobs/unpaid',getProfile ,async (req, res) =>{
                 }
            }]
     })
-    if(!contracts) return res.status(404).end()
-    res.json(contracts)
+    if(!jobs) return res.status(404).end()
+    res.json(jobs)
 })
 
-
+// 2
 app.get('/contracts',getProfile ,async (req, res) =>{
     const {Contract} = req.app.get('models')
     const clientId = req.profile.id
@@ -60,6 +101,7 @@ app.get('/contracts',getProfile ,async (req, res) =>{
     res.json(contracts)
 })
 
+// 1
 app.get('/contracts/:id',getProfile ,async (req, res) =>{
     const {Contract} = req.app.get('models')
     const { id: contractId } = req.params
